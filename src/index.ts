e500d10675da451769e7ee68c0c8f5b75dcc652a
nyxb <contact@nyxb.email>
2023-09-30 08:13:45 +0200

    docs: add more docs

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,27 @@ import namedTupleSpacing from './rules/named-tuple-spacing'
 import consistentListNewline from './rules/consistent-list-newline'
 
 export default {
-  rules: {
-    'consistent-list-newline': consistentListNewline,
-    'generic-spacing': genericSpacing,
-    'if-newline': ifNewline,
-    'import-dedupe': importDedupe,
-    'named-tuple-spacing': namedTupleSpacing,
-    'no-cjs-exports': noCjsExports,
-    'no-const-enum': noConstEnum,
-    'no-import-node-modules-by-path': noImportNodeModulesByPath,
-    'no-ts-export-equal': noTsExportEqual,
-    'prefer-inline-type-import': preferInlineTypeImport,
-    'top-level-function': topLevelFunction,
+   rules: {
+      'consistent-list-newline': consistentListNewline,
+      'generic-spacing': genericSpacing,
+      'if-newline': ifNewline,
+      'import-dedupe': importDedupe,
+      'named-tuple-spacing': namedTupleSpacing,
+      'no-cjs-exports': noCjsExports,
+      'no-const-enum': noConstEnum,
+      'no-import-node-modules-by-path': noImportNodeModulesByPath,
+      'no-ts-export-equal': noTsExportEqual,
+      'prefer-inline-type-import': preferInlineTypeImport,
+      'top-level-function': topLevelFunction,
 
-    // deprecated
-    'consistent-object-newline': consistentListNewline,
-  },
+      // deprecated
+      'consistent-object-newline': {
+         ...consistentListNewline,
+         name: 'consistent-object-newline',
+         meta: {
+            ...consistentListNewline.meta,
+            deprecated: true,
+         },
+      } as typeof consistentListNewline,
+   },
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,16 @@
 import { ESLintUtils } from '@typescript-eslint/utils'
 
+const hasDocs = [
+   'consistent-list-newline',
+   'if-newline',
+   'import-dedupe',
+   'top-level-function',
+]
+
+const blobUrl = 'https://github.com/nyxb/eslint-plugin-nyxb/blob/main/src/rules/'
+
 export const createEslintRule = ESLintUtils.RuleCreator(
-  ruleName => ruleName,
+   ruleName => hasDocs.includes(ruleName)
+      ? `${blobUrl}${ruleName}.md`
+      : `${blobUrl}${ruleName}.test.ts`,
 )
