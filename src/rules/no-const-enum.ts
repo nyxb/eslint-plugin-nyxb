943a51f4fc4719e9f7ef447df72b258f9e600ff2
nyxb <contact@nyxb.email>
2023-10-18 11:14:35 +0200

    🔧 refactor(consistent-list-newline.test.ts.snap): add new test cases to improve coverage 🔧 refactor(no-const-enum.ts): deprecate rule and suggest alternative to align with ESLint best practices

--- src/rules/no-const-enum.ts
+++ src/rules/no-const-enum.ts
@@ -5,29 +5,29 @@ export type MessageIds = 'noConstEnum'
 export type Options = []
 
 export default createEslintRule<Options, MessageIds>({
-  name: RULE_NAME,
-  meta: {
-    type: 'problem',
-    docs: {
-      description: 'Disallow using `const enum` expression',
-      recommended: 'recommended',
-    },
-    schema: [],
-    messages: {
-      noConstEnum: 'Do not use `const enum` expression',
-    },
-  },
-  defaultOptions: [],
-  create: (context) => {
-    return {
-      TSEnumDeclaration: (node) => {
-        if (node.const) {
-          context.report({
-            node,
-            messageId: 'noConstEnum',
-          })
-        }
+   name: RULE_NAME,
+   meta: {
+      type: 'problem',
+      docs: {
+         description: 'Deprecated. Use `\'no-restricted-syntax\': [\'error\', \'TSEnumDeclaration[const=true]\']` instead.',
       },
-    }
-  },
+      schema: [],
+      messages: {
+         noConstEnum: 'Do not use `const enum` expression',
+      },
+      deprecated: true,
+   },
+   defaultOptions: [],
+   create: (context) => {
+      return {
+         TSEnumDeclaration: (node) => {
+            if (node.const) {
+               context.report({
+                  node,
+                  messageId: 'noConstEnum',
+               })
+            }
+         },
+      }
+   },
 })
